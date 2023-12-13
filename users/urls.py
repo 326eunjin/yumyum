fb67a2864b1a8b393f5f5d86d693f777e44d0642
정한수 <jounghs3933@gmail.com>
2023-12-14 02:32:04 +0900

    마이페이지 url 변경

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('auth/', AuthView.as_view()),
     path('auth/verify/', TokenVerifyView.as_view()),
     path('auth/refresh/', TokenRefreshView.as_view()),
-    path('<int:user_id>/', UserInfoView.as_view()),
+    path('mypage/', UserInfoView.as_view()),
     path('waitings/', UserWaitingView.as_view()),
     path('<int:user_id>/reviews/', UserReviewListView.as_view()),           #유저 리뷰 조회
     path('<int:user_id>/reviews/<int:review_id>', DeleteReview.as_view()),  #리뷰 삭제
